5d36f7cae5a1d1f162dfd492569d8ec85b586944
Vlad <->
2023-05-02 00:30:28 +0300

    - Remove migration assembly from option.UseSqlServer

--- Shop.API/Program.cs
+++ Shop.API/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ShopContext>(options =>
 {
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("Shop.Infrastructure"));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
